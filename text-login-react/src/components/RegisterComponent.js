fea02d87c88c031e2306de84da25835ae5ea949e
Brad Cantrell <brad777mx@hotmail.com>
2019-05-14 14:31:38 -0700

    more react code

---
+++ text-login-react/src/components/RegisterComponent.js
@@ -0,0 +1,51 @@
+import React from "react"
+import axios from "axios"
+
+class LoginComponent extends React.Component {
+    constructor() {
+        super()
+
+        this.state = {
+            username: "",
+            password: ""
+        }
+    }
+
+    register = e => {
+        e.preventDefault()
+
+        axios
+            .post("http://localhost:5000/api/register", this.state)
+            .then(res => console.log(res).catch(err => console.log(err)))
+    }
+
+    handleInput = e => {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.register}>
+                    <input
+                        type="text"
+                        name="username"
+                        value={this.state.username}
+                        onChange={this.handleInput}
+                        placeholder="username"
+                    />
+                    <input
+                        type="text"
+                        name="password"
+                        value={this.state.password}
+                        onChange={this.handleInput}
+                        placeholder="password"
+                    />
+                    <button type="submit">submit</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default LoginComponent
